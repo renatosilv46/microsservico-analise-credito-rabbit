597981e4be12362945887d96b2c2e85e713a302a
Renato <rnt.graca01@gmail.com>
2025-03-22 01:04:04 -0300

    improv: Ajuste na utilização do lombok

--- src/main/java/com/analise/credito/domain/Proposta.java
+++ src/main/java/com/analise/credito/domain/Proposta.java
@@ -1,5 +1,10 @@
 package com.analise.credito.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Proposta {
     private Long id;
     private double valorSolicitado;
@@ -8,60 +13,4 @@ public class Proposta {
     private boolean integrada;
     private String observacao;
     private Usuario usuario;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public double getValorSolicitado() {
-        return valorSolicitado;
-    }
-
-    public void setValorSolicitado(double valorSolicitado) {
-        this.valorSolicitado = valorSolicitado;
-    }
-
-    public int getPrazoPagamento() {
-        return prazoPagamento;
-    }
-
-    public void setPrazoPagamento(int prazoPagamento) {
-        this.prazoPagamento = prazoPagamento;
-    }
-
-    public Boolean getAprovada() {
-        return aprovada;
-    }
-
-    public void setAprovada(Boolean aprovada) {
-        this.aprovada = aprovada;
-    }
-
-    public boolean isIntegrada() {
-        return integrada;
-    }
-
-    public void setIntegrada(boolean integrada) {
-        this.integrada = integrada;
-    }
-
-    public String getObservacao() {
-        return observacao;
-    }
-
-    public void setObservacao(String observacao) {
-        this.observacao = observacao;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
 }
--- src/main/java/com/analise/credito/domain/Usuario.java
+++ src/main/java/com/analise/credito/domain/Usuario.java
@@ -1,5 +1,10 @@
 package com.analise.credito.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Usuario {
     private Long id;
     private String nome;
@@ -7,52 +12,4 @@ public class Usuario {
     private String cpf;
     private String telefone;
     private Double renda;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public Double getRenda() {
-        return renda;
-    }
-
-    public void setRenda(Double renda) {
-        this.renda = renda;
-    }
 }
