3d09a3687dcf1acdb6be3d93213015eceb497a88
Renato <rnt.graca01@gmail.com>
2025-03-24 22:04:18 -0300

    improv: Implementação dos logs na classe main

--- src/main/java/com/analise/credito/MsAnaliseDeCreditoApplication.java
+++ src/main/java/com/analise/credito/MsAnaliseDeCreditoApplication.java
@@ -1,15 +1,14 @@
 package com.analise.credito;
 
-import com.analise.credito.domain.Proposta;
-import com.analise.credito.service.analiseCredito.AnaliseCreditoService;
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class MsAnaliseDeCreditoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsAnaliseDeCreditoApplication.class, args);
+		log.info("Servidor inicializado com sucesso na porta: 8080");
 	}
 }
